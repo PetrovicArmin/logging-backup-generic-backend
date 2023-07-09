6ff01f2f5013cb91da73c2dd38c1731d99ab0619
Armin Petrović <apetrovic1@etf.unsa.ba>
2023-07-09 23:52:36 +0200

    Added logging of our api communication, as well as errors on our backend

--- api/server.ts
+++ api/server.ts
@@ -7,6 +7,8 @@ import path from "path";
 import { Container } from "typedi";
 import { ProductController } from "./controllers/applied/product.controller.js";
 import { SkuController } from "./controllers/applied/sku.controller.js";
+import winston from 'winston';
+import expressWinston from 'express-winston';
 
 const __filename: string = fileURLToPath(import.meta.url);
 const __dirname: string = path.dirname(__filename);
@@ -20,6 +22,23 @@ const app: Express = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
+//api logger
+expressWinston.requestWhitelist.push('body'); //be careful of this data!
+expressWinston.responseWhitelist.push('body');
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.File({ filename: "api.log", dirname: path.join(__dirname, '..', 'logs') })
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json(),
+    winston.format.timestamp(),
+    winston.format.prettyPrint()
+  ),
+  expressFormat: true
+}));
+
 //ioc
 let productController = Container.get(ProductController);
 let skuController = Container.get(SkuController);
@@ -27,6 +46,19 @@ let skuController = Container.get(SkuController);
 //routes
 app.use('/products', productController.getRouter());
 app.use('/skus', skuController.getRouter());
+
+//error logger
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.File({ filename: 'errors.log', dirname: path.join(__dirname, '..', 'logs') })
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json(),
+    winston.format.timestamp(),
+    winston.format.prettyPrint()
+  )
+}));
 
 const port: number = 8080; 
 app.listen(port, () => {
