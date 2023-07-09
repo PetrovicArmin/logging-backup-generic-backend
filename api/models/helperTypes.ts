030ed250dc8e8d4969dd7804ac081e71f96b1f6f
Armin Petrović <apetrovic1@etf.unsa.ba>
2023-07-09 23:22:44 +0200

    Finished api routes for mock backend

--- api/models/helperTypes.ts
+++
@@ -1,5 +0,0 @@
-import { Model } from "sequelize";
-
-// Type `ModelType` would basically wrap & satisfy the 'this' context of any sequelize helper methods
-export type Constructor<T> = new (...args: any[]) => T;
-export type ModelType<T extends Model<T, any>> = Constructor<T> & typeof Model;
